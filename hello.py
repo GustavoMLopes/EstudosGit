f9cf6da952d58485dbaa3673576995e76f0edc0a
Gustavo Martins <gumartinslopes@gmail.com>
2023-01-09 18:42:10 -0300

    udpate greet

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
+def greet(name1, name2):
+    print(f'{name1} says hello to {name2}')
+
 def greeter(name):
     print(f'Hello {name}!')
     
 def say(word):
     print(word)
-    
-greeter('Bob')
+
+greet('Bob', 'Greg')
